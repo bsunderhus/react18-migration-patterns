f1ecf851745cdd0e18bd98c2259d59ba6861ce40
Bernardo Sunderhus <bernardo.sunderhus@gmail.com>
2025-12-05 11:50:15 +0100

    feature: storybook init

---
+++ .storybook/preview.ts
@@ -0,0 +1,11 @@
+import type { Preview } from "@storybook/react-vite";
+
+const preview: Preview = {
+  parameters: {
+    docs: {
+      codePanel: true,
+    },
+  },
+};
+
+export default preview;
